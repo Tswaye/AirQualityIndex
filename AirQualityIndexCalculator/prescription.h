da3cbf4b7476314624389272fad69c62bac13f24
Tswaye <114791031+Tswaye@users.noreply.github.com>
2022-11-30 20:33:21 -0800

    Add files via upload

---
+++ AirQualityIndexCalculator/prescription.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+
+void prescription(float e)                  //prescription as per the value of AQI
+{
+	if (e >= 0 && e <= 50)
+	{
+		cout << "\nAQI of the AIR is=" << e;
+		cout << "\n";
+		cout << "\nPrescription:";
+		cout << "\nAir Quality is satisfactory and poses little or no health risk.";
+	}
+	if (e >= 51 && e <= 100)
+	{
+		cout << "\nAQI of the AIR is=" << e;
+		cout << "\n";
+		cout << "\nPrescription:";
+		cout << "\nAir quality is acceptable;however in this range may pose a moderate health concern for a small number of individuals.";
+	}
+	if (e >= 101 && e <= 150)
+	{
+		cout << "\nAQI of the AIR is=" << e;
+		cout << "\n";
+		cout << "\nPrescription:";
+		cout << "\nFor this value of AQI,members of sensitive groups may experience health effects, but the general public is unlikely to be affected";
+	}
+	if (e > 151 && e <= 200)
+	{
+		cout << "\nAQI of the AIR is=" << e;
+		cout << "\n";
+		cout << "\nPrescription:";
+		cout << "\nEveryone may begin to experience health effects for this value of AQI.Members of sensitive groups may experience more serious health effects";
+	}
+	if (e >= 201 && e <= 300)
+	{
+		cout << "\nAQI of the AIR is=" << e;
+		cout << "\n";
+		cout << "\nPrescription:";
+		cout << "\nThis AQI value may trigger a health alert,meaning everyone may experience more serious health effects";
+	}
+	if (e >= 301 && e <= 500)
+	{
+		cout << "\nAQI of the AIR is=" << e;
+		cout << "\n";
+		cout << "\nPrescription:";
+		cout << "\nAQi values over 300 trigger health warnings of emergency conditions. The entire population is even more likely to be affected by serious health effects";
+	}
+}
